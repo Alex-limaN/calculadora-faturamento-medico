765d2e915e1d2b66cb43a0f0aaa78900e197e269
Alex-limaN <alexlima6700@gmail.com>
2026-07-31 21:22:44 -0300

    Fix: Atualiza rotas

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from api.views import CalculadoraView # Remova a ListarConveniosView daqui
+from api.views import CalculadoraView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/calcular/', CalculadoraView.as_view(), name='calcular'),
-    # Remova também a linha que usava a ListarConveniosView, se ela existir abaixo
+    path('api/calcular/', CalculadoraView.as_view(), name='calcular'), 
 ]
